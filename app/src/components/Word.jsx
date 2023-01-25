afc9d86e4bb7eb62b2df42f9d8d6b7428c683d23
Graham Theobald <Graham_Theobald@live.co.uk>
2023-01-25 16:36:03 +0000

    audio file plays

--- app/src/components/Word.jsx
+++ app/src/components/Word.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Definition from './Definition'
 import play from '../assets/images/icon-play.svg'
 import newWindow from '../assets/images/icon-new-window.svg'
@@ -6,6 +6,8 @@ import './Word.css'
 
 export default function Word({ data }) {
 	const { word, phonetics, meanings, sourceUrls } = data
+	const [audio, setAudio] = useState(phonetics[0].audio)
+	const audioRef = useRef(null)
 	return (
 		<div className='info'>
 			<div className='info__header'>
@@ -23,7 +25,13 @@ export default function Word({ data }) {
 							})}
 					</ul>
 				</div>
-				<img className='header__audio' src={play} alt='play arrow' />
+				<img
+					onClick={() => audioRef.current.play()}
+					className='header__audio'
+					src={play}
+					alt='play arrow'
+				/>
+				<audio ref={audioRef} src={audio} />
 			</div>
 			{meanings.map((meaning, i) => {
 				return (
@@ -35,7 +43,7 @@ export default function Word({ data }) {
 						<p className='meaning__meaning'>Meaning</p>
 						<ul className='definition__list'>
 							{meaning.definitions.map((definition, i) => {
-								return <Definition kay={i} Definition={definition} />
+								return <Definition key={i} Definition={definition} />
 							})}
 						</ul>
 						{meaning.synonyms.length > 0 && (
